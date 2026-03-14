f93738dd9ae596c0ede9c62ef9780b1fb2299926
Andre Kurait <andrekurait@gmail.com>
2026-03-14 11:06:42 +0000

    Add S3 backend for terraform state persistence
    Signed-off-by: Andre Kurait <andrekurait@gmail.com>

--- deploy/terraform/providers.tf
+++ deploy/terraform/providers.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.5"
 
+  backend "s3" {
+    bucket = "temporalci-tfstate-<ACCOUNT_ID>"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
