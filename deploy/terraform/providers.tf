6fbe7f0a02a7fbb397f44abc3501d29cfb6e19de
Andre Kurait <andrekurait@gmail.com>
2026-03-19 18:20:32 +0000

    infra: manage ArgoCD via Terraform Helm provider
    - ArgoCD installed as Helm release (argo-cd chart v7.7.16)
    - TemporalCI Application resource with automated sync + self-heal
    - Helm + Kubernetes providers configured against EKS cluster
    - Replaces manually-applied ArgoCD manifests with IaC
    
    Signed-off-by: Andre Kurait <andrekurait@gmail.com>

--- deploy/terraform/providers.tf
+++ deploy/terraform/providers.tf
@@ -12,9 +12,35 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
   }
 }
 
 provider "aws" {
   region = var.region
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = aws_eks_cluster.this.name
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
 }
