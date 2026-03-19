e56ef05eca51d442712144643c357da5283581cb
Andre Kurait <andrekurait@gmail.com>
2026-03-19 19:17:19 +0000

    infra: use EKS ArgoCD Capability (aws_eks_capability)
    - Replace Helm-managed ArgoCD with native EKS ArgoCD Capability
    - EKS provisions and manages ArgoCD outside the cluster
    - Integrated with IAM Identity Center for authentication
    - TemporalCI Application resource with automated sync + self-heal
    
    Signed-off-by: Andre Kurait <andrekurait@gmail.com>

--- deploy/terraform/providers.tf
+++ deploy/terraform/providers.tf
@@ -12,10 +12,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.0"
-    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.0"
@@ -35,12 +31,4 @@ provider "kubernetes" {
   host                   = aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.this.token
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.this.token
-  }
 }
