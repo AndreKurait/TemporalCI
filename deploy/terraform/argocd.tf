b8f08446c2447b719e5c5b289688a227b095497b
Andre Kurait <andrekurait@gmail.com>
2026-03-19 19:17:19 +0000

    infra: use EKS ArgoCD Capability (aws_eks_capability)
    - Replace Helm-managed ArgoCD with native EKS ArgoCD Capability
    - EKS provisions and manages ArgoCD outside the cluster
    - Integrated with IAM Identity Center for authentication
    - TemporalCI Application resource with automated sync + self-heal
    
    Signed-off-by: Andre Kurait <andrekurait@gmail.com>

--- deploy/terraform/argocd.tf
+++ deploy/terraform/argocd.tf
@@ -1,27 +1,68 @@
-# --- ArgoCD (Helm-managed, GitOps for TemporalCI) ---
+# --- ArgoCD EKS Capability ---
+# Uses the native EKS ArgoCD Capability (aws_eks_capability)
+# EKS provisions and manages ArgoCD outside the cluster, reducing operational overhead.
 
-resource "helm_release" "argocd" {
-  name             = "argocd"
-  repository       = "https://argoproj.github.io/argo-helm"
-  chart            = "argo-cd"
-  version          = "7.7.16"
-  namespace        = "argocd"
-  create_namespace = true
+data "aws_ssoadmin_instances" "this" {}
 
-  set {
-    name  = "server.service.type"
-    value = "ClusterIP"
+resource "aws_iam_role" "argocd_capability" {
+  name = "${var.cluster_name}-argocd-capability"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "argocd_capability" {
+  name = "argocd-capability"
+  role = aws_iam_role.argocd_capability.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["secretsmanager:GetSecretValue"]
+        Resource = "arn:aws:secretsmanager:${var.region}:${data.aws_caller_identity.current.account_id}:secret:temporalci/*"
+      }
+    ]
+  })
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_eks_capability" "argocd" {
+  cluster_name              = aws_eks_cluster.this.name
+  capability_name           = "argocd"
+  type                      = "ARGOCD"
+  role_arn                  = aws_iam_role.argocd_capability.arn
+  delete_propagation_policy = "RETAIN"
+
+  configuration {
+    argo_cd {
+      namespace = "argocd"
+
+      aws_idc {
+        idc_instance_arn = tolist(data.aws_ssoadmin_instances.this.arns)[0]
+        idc_region       = var.region
+      }
+    }
   }
 
-  set {
-    name  = "configs.params.server\\.insecure"
-    value = "true"
+  tags = {
+    Project = "TemporalCI"
   }
 
   depends_on = [aws_eks_cluster.this]
 }
 
-# --- ArgoCD Application for TemporalCI (self-managing GitOps) ---
+# --- ArgoCD Application for TemporalCI (GitOps self-managing) ---
 
 resource "kubernetes_manifest" "temporalci_app" {
   manifest = {
@@ -55,5 +96,5 @@ resource "kubernetes_manifest" "temporalci_app" {
     }
   }
 
-  depends_on = [helm_release.argocd]
+  depends_on = [aws_eks_capability.argocd]
 }
